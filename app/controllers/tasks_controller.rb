b44ccfdab5b5d6cb3888a8b702286957a0146482
genki.mikita <mickybsk1031@outlook.jp>
2020-01-26 12:58:27 +0000

    miki100

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,13 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
-    end
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def show
-    
+    @task = Task.find(params[:id])
   end
   
   def new
@@ -56,7 +53,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-  def set_task
+  def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
